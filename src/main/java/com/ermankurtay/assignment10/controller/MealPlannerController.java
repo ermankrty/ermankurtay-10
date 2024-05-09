8f3fa1efb9e0ae537dfef034d1825950d750bbee
ermankrty <kurtayerman@gmail.com>
2024-05-08 22:16:48 -0400

    fixed white label error
    removed spoonacular api class and merged to controller

--- src/main/java/com/ermankurtay/assignment10/controller/MealPlannerController.java
+++ src/main/java/com/ermankurtay/assignment10/controller/MealPlannerController.java
@@ -1,36 +1,61 @@
 package com.ermankurtay.assignment10.controller;
 
-import com.ermankurtay.assignment10.model.DayResponse;
-import com.ermankurtay.assignment10.model.WeekResponse;
-import com.ermankurtay.assignment10.spoonacular.SpoonacularIntegration;
-
+import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.ermankurtay.assignment10.model.DayResponse;
+import com.ermankurtay.assignment10.model.WeekResponse;
 
 @RestController
 public class MealPlannerController {
 
-    private final SpoonacularIntegration spoonacularIntegration;
+    private static final String API_KEY = "1a5d951cbf50420aaf54751c55e6b2dd";
+    private static final String SPOONACULAR_API_URL = "https://api.spoonacular.com/mealplanner/generate";
 
-    public MealPlannerController(SpoonacularIntegration spoonacularIntegration) {
-        this.spoonacularIntegration = spoonacularIntegration;
+    @GetMapping("/mealplanner/day")
+    public ResponseEntity<DayResponse> getDayMeals(
+            @RequestParam(value = "targetCalories", required = false) String numCalories,
+            @RequestParam(required = false) String diet,
+            @RequestParam(value = "exclude", required = false) String exclusions) {
+
+        URI uri = buildUri("day", numCalories, diet, exclusions);
+
+        return new RestTemplate().getForEntity(uri, DayResponse.class);
     }
 
     @GetMapping("/mealplanner/week")
     public ResponseEntity<WeekResponse> getWeekMeals(
-            @RequestParam String numCalories,
-            @RequestParam String diet,
-            @RequestParam String exclusions) {
-        return spoonacularIntegration.getWeekMeals(numCalories, diet, exclusions);
+            @RequestParam(value = "targetCalories", required = false) String numCalories,
+            @RequestParam(required = false) String diet,
+            @RequestParam(value = "exclude", required = false) String exclusions) {
+
+        URI uri = buildUri("week", numCalories, diet, exclusions);
+
+        return new RestTemplate().getForEntity(uri, WeekResponse.class);
     }
 
-    @GetMapping("/mealplanner/day")
-    public ResponseEntity<DayResponse> getDayMeals(
-            @RequestParam String numCalories,
-            @RequestParam String diet,
-            @RequestParam String exclusions) {
-        return spoonacularIntegration.getDayMeals(numCalories, diet, exclusions);
+    private URI buildUri(String timeFrame, String numCalories, String diet, String exclusions) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(SPOONACULAR_API_URL)
+                .queryParam("apiKey", API_KEY)
+                .queryParam("timeFrame", timeFrame);
+
+        if (numCalories != null) {
+            builder.queryParam("targetCalories", numCalories);
+        }
+
+        if (diet != null) {
+            builder.queryParam("diet", diet);
+        }
+
+        if (exclusions != null) {
+            builder.queryParam("exclude", exclusions);
+        }
+
+        return builder.build().toUri();
     }
 }
